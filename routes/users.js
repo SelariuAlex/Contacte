b1cff2b5d8ef5f3cb93bb14589388de5280f2660
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-08-28 10:47:06 +0300

    User schema and user validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 //POST api/users
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 4 or more characters'
+    ).isLength({ min: 4 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect DB
 connectDB();
 
+// middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Adauga contacte' }));
 
 //Routes
