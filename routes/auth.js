fa4b348a4615b6c9980cf9124afd38984dcaa836
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-08-29 10:56:20 +0300

    auth Middleware, protecting Routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,19 @@ const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
 //GET api/auth
-router.get('/', (req, res) => {
-  res.send('get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //POST api/auth
