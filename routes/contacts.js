b950262cb24a4884bd8a4365d3014181838217ba
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-08-29 12:47:33 +0300

    add contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -20,9 +20,44 @@ router.get('/', auth, async (req, res) => {
 });
 
 //POST api/contacts
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+    };
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //PUT api/contacts/:id
 router.put('/:id', (req, res) => {
