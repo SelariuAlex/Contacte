13d7e0246bdab5a2590e953628526dd0fd675ea7
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-01 13:31:14 +0300

    delete contact

--- client/src/context/contact/Contact.state.js
+++ client/src/context/contact/Contact.state.js
@@ -50,8 +50,15 @@ const ContactState = props => {
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
 
+  // Delete contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
+    <ContactContext.Provider
+      value={{ contacts: state.contacts, addContact, deleteContact }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
