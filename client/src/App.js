f646017ccb5a06fbde1043e5a48f5df12b9bcf29
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-04 11:39:14 +0300

    add auth context

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/Contact.state';
+import AuthState from './context/auth/Auth.state';
 
 import './App.css';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
