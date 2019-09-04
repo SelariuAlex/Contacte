935c47d8753cac521b5bce9ae268ee75e490b09d
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-09-04 17:05:11 +0300

    add alerts

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/Contact.state';
 import AuthState from './context/auth/Auth.state';
+import AlertState from './context/alert/Alert.state';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import './App.css';
 
@@ -14,19 +16,22 @@ function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
